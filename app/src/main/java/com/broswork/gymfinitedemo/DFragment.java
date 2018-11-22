cd7f9e7a3eb1716efd660ec01fb9b8e19951a34a
subashc069 <bcoolboy8@gmail.com>
2018-11-23 00:10:38 +0545

    checkin layout and pop up

---
+++ app/src/main/java/com/broswork/gymfinitedemo/DFragment.java
@@ -0,0 +1,19 @@
+package com.broswork.gymfinitedemo;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DFragment extends DialogFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.dialog_layout, container,
+                false);
+        //getDialog().setTitle("DialogFragment Tutorial");
+        // Do something else
+        return rootView;
+    }
+}
--- app/src/main/java/com/broswork/gymfinitedemo/MainActivity.java
+++ app/src/main/java/com/broswork/gymfinitedemo/MainActivity.java
@@ -1,15 +1,36 @@
 package com.broswork.gymfinitedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    Button check_inButton;
+    FragmentManager fm = getSupportFragmentManager();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
+        // Get the view from activity_main.xml
+        setContentView(R.layout.searched_checkin_layout2);
+
+        // Locate the button in activity_main.xml
+        check_inButton = (Button) findViewById(R.id.check_inButton);
+
+
+        // Capture button clicks
+        check_inButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                DFragment dFragment = new DFragment();
+                // Show DialogFragment
+                dFragment.show(fm, "Dialog Fragment");
+            }
+        });
+
+
     }
 }
-
-
