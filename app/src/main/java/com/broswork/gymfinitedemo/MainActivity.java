ba07e9655e1623bd022c5776c65ebcc71998c05a
subashc069 <bcoolboy8@gmail.com>
2018-11-23 01:31:31 +0545

    settings layout added

--- app/src/main/java/com/broswork/gymfinitedemo/MainActivity.java
+++ app/src/main/java/com/broswork/gymfinitedemo/MainActivity.java
@@ -1,35 +1,23 @@
 package com.broswork.gymfinitedemo;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends FragmentActivity {
 
-    Button check_inButton;
-    FragmentManager fm = getSupportFragmentManager();
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Get the view from activity_main.xml
-        setContentView(R.layout.searched_checkin_layout2);
-
-        // Locate the button in activity_main.xml
-        check_inButton = (Button) findViewById(R.id.check_inButton);
+        setContentView(R.layout.setting_layout);
 
 
-        // Capture button clicks
-        check_inButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View arg0) {
-                DFragment dFragment = new DFragment();
-                // Show DialogFragment
-                dFragment.show(fm, "Dialog Fragment");
-            }
-        });
 
 
     }
---
+++ app/src/main/java/com/broswork/gymfinitedemo/SearchedLoginActivity.java
@@ -0,0 +1,36 @@
+package com.broswork.gymfinitedemo;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SearchedLoginActivity extends FragmentActivity {
+
+    Button check_inButton;
+    FragmentManager fm = getSupportFragmentManager();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from activity_main.xml
+        setContentView(R.layout.searched_checkin_layout2);
+
+        // Locate the button in activity_main.xml
+        check_inButton = (Button) findViewById(R.id.check_inButton);
+
+
+        // Capture button clicks
+        check_inButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+                DFragment dFragment = new DFragment();
+                // Show DialogFragment
+                dFragment.show(fm, "Dialog Fragment");
+            }
+        });
+
+
+    }
+}
