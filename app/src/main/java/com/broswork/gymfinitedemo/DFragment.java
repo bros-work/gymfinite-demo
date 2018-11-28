a72b6ae17ea6b2e9f6f18515b7be6c34e947cb4a
cmanish049 <cmanish049@gmail.com>
2018-11-28 19:33:21 +0545

    ok btn background fixes

--- app/src/main/java/com/broswork/gymfinitedemo/DFragment.java
+++ app/src/main/java/com/broswork/gymfinitedemo/DFragment.java
@@ -1,12 +1,14 @@
 package com.broswork.gymfinitedemo;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class DFragment extends DialogFragment {
@@ -16,7 +18,14 @@ public class DFragment extends DialogFragment {
         View rootView = inflater.inflate(R.layout.dialog_layout, container,
                 false);
 
-
+        Button mBtnOk = rootView.findViewById(R.id.login_success);
+        mBtnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(getContext(),SettingActivity.class);
+                getContext().startActivity(i);
+            }
+        });
 
 
         return rootView;
