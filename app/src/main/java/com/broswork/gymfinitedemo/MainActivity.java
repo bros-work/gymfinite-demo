cfb8bdfe6e0a1e3cb3f471cfbdd83cc555b4f89d
cmanish049 <cmanish049@gmail.com>
2018-12-03 19:47:09 +0545

    implemented mlkit

--- app/src/main/java/com/broswork/gymfinitedemo/MainActivity.java
+++ app/src/main/java/com/broswork/gymfinitedemo/MainActivity.java
@@ -1,6 +1,12 @@
 package com.broswork.gymfinitedemo;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +17,20 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +64,73 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(getApplicationContext(),"login",Toast.LENGTH_LONG).show();
-                Intent myIntent = new Intent(MainActivity.this, CheckinActivity.class);
+//                Intent myIntent = new Intent(MainActivity.this, CheckinActivity.class);
                 //myIntent.putExtra("key", value); //Optional parameters
-                MainActivity.this.startActivity(myIntent);
+//                MainActivity.this.startActivity(myIntent);
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
         }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, requestCode, Toast.LENGTH_SHORT).show();
+        if (requestCode == REQUEST_IMAGE_CAPTURE ) {
+            if (resultCode == RESULT_OK) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                barcodeRecognition(photo);
+            }
+        }
+    }
 
+    private void barcodeRecognition(Bitmap photo) {
+        FirebaseVisionBarcodeDetectorOptions options =
+                new FirebaseVisionBarcodeDetectorOptions.Builder()
+                        .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_ALL_FORMATS)
+                        .build();
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(photo);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector();
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                        for (FirebaseVisionBarcode barcode: barcodes) {
+                            Rect bounds = barcode.getBoundingBox();
+                            Point[] corners = barcode.getCornerPoints();
 
+                            String rawValue = barcode.getRawValue();
+
+                            int valueType = barcode.getValueType();
+                            Toast.makeText(MainActivity.this, ""+rawValue, Toast.LENGTH_SHORT).show();
+                            // See API reference for complete list of supported types
+//                            switch (valueType) {
+//                                case FirebaseVisionBarcode.TYPE_WIFI:
+//                                    String ssid = barcode.getWifi().getSsid();
+//                                    String password = barcode.getWifi().getPassword();
+//                                    int type = barcode.getWifi().getEncryptionType();
+//                                    break;
+//                                case FirebaseVisionBarcode.TYPE_URL:
+//                                    String title = barcode.getUrl().getTitle();
+//                                    String url = barcode.getUrl().getUrl();
+//                                    break;
+//                            }
+                        }
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
 
 
